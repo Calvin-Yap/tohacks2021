75dea098b1c3321cc913c2e2c9ded03aa68eaddc
Calvin Yap <calvin.yap@ryerson.ca>
2021-05-08 11:03:51 -0400

    added image

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.imagescanada.ca/wp-content/uploads/2019/03/Spectacular-Photos-of-Niagara-Falls-Casinos.jpg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
