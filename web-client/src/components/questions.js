0bcabb16de669092a30ccf8d5c8aa4936461bef3
Lawrence Mak <maklawrence1110@gmail.com>
2021-05-08 17:41:47 -0400

    v1.0

--- web-client/src/components/questions.js
+++ web-client/src/components/questions.js
@@ -1,108 +1,119 @@
-import React from "react";
-  import { makeStyles } from "@material-ui/core/styles";
-  import Typography from "@material-ui/core/Typography";
-  import FormGroup from '@material-ui/core/FormGroup';
-  import FormControlLabel from '@material-ui/core/FormControlLabel';
-  import Checkbox from '@material-ui/core/Checkbox';
-  import Slider from "@material-ui/core/Slider";
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Slider from "@material-ui/core/Slider";
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      width: 150
-    },
-    margin: {
-      height: theme.spacing(1)
-    }
-  }));
-
-  const marks = [
-    {
-      value: 0,
-      label: "Bored"
-    },
-    {
-      value: 25,
-      label: "Snack"
-    },
-    {
-      value: 50,
-      label: "Eh"
-    },
-    {
-      value: 75,
-      label: "Hungry"
-    },
-    {
-      value: 100,
-      label: "Famished"
-    }
-  ];
-
-  function valuetext(value) {
-    return `${value}°C`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 300
+  },
+  margin: {
+    height: theme.spacing(1)
   }
-const questions=()=> { 
+}));
 
-  
+const marks = [
+  {
+    value: 0,
+    label: "Bored"
+  },
+  {
+    value: 25,
+    label: "Snack"
+  },
+  {
+    value: 50,
+    label: "Eh"
+  },
+  {
+    value: 75,
+    label: "Hungry"
+  },
+  {
+    value: 100,
+    label: "Famished"
+  }
+];
+
+function valuetext(value) {
+  return `${value}$`;
+}
+
+const Questions = () => {
+  const [value, setValue] = useState([20, 37]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const [tag, setTag] = useState([]);
+  const handleClick = (newTag) => {
+    setTag((prev) => {
+      console.log(tag);
+      return [...prev, newTag];
+      
+    });
+  };
+
   return (
     <div>
       <div>
-      <Typography id="discrete-slider-custom" gutterBottom>
-        How Hungry are you
-      </Typography>
-      <Slider
-        defaultValue={50}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider-custom"
-        step={25}
-        marks={marks}
-      />
+        <Typography id="discrete-slider-custom" gutterBottom>
+          How Hungry are you
+        </Typography>
+        <Slider
+          defaultValue={50}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider-custom"
+          step={25}
+          marks={marks}
+        />
       </div>
 
       <div>
-      <FormGroup row>
-      <FormControlLabel
-        control={<Checkbox name="checkedA" />}
-        label="Savory"
+        <FormGroup row>
+          <FormControlLabel
+            control={<Checkbox name="checkedA" onClick={(e)=>handleClick('Savory')} />}
+            label="Savory"
+          />
 
-      />
-      
-      <FormControlLabel
-        control={<Checkbox name="checkedA" />}
-        label="Sweet"
-      />   
+          <FormControlLabel
+            control={<Checkbox name="checkedA" onClick={(e)=>handleClick('Sweet')}/>}
+            label="Sweet"
+          />
 
-      <FormControlLabel
-        control={<Checkbox name="checkedA" />}
-        label="Spicy"
-      />   
+          <FormControlLabel
+            control={<Checkbox name="checkedA" onClick={(e)=>handleClick('Spicy')}/>}
+            label="Spicy"
+          />
 
-      <FormControlLabel
-        control={<Checkbox name="checkedA" />}
-        label="Refreshing"
-      />   
-      <FormControlLabel
-        control={<Checkbox name="checkedA" />}
-        label="Anything"
-      />   
-    </FormGroup>
+          <FormControlLabel
+            control={<Checkbox name="checkedA" onClick={(e)=>handleClick('Refreshing')}/>}
+            label="Refreshing"
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedA" onClick={(e)=>handleClick('Anything')}/>}
+            label="Anything"
+          />
+        </FormGroup>
       </div>
 
-    <div>
-    <Typography id="range-slider" gutterBottom>
-  Budget
-</Typography>
-<Slider
-//  value={value}
-//  onChange={handleChange}
-//  valueLabelDisplay="auto"
-//  aria-labelledby="range-slider"
-  getAriaValueText={valuetext}
-/>
-    </div>
+      <div>
+        <Typography id="range-slider" gutterBottom>
+          Budget
+        </Typography>
+        <Slider
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={valuetext}
+        />
+      </div>
     </div>
   );
-}
+};
 
-
-export default questions;
+export default Questions;
--- web-client/src/index.js
+++ web-client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Questions from './components/questions'
 import { Themeprovider } from '@material-ui/styles'
+import questions from './components/questions';
 ReactDOM.render(
 
     <Questions  /> ,
